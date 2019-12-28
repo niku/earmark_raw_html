1d98821d53a5570d80d3e6d38cb95ccff5c3ed20
niku <niku@niku.name>
2019-12-29 01:53:40 +0900

    Create a project
    $ mix new earmark_raw_html

---
+++ lib/earmark_raw_html.ex
@@ -0,0 +1,18 @@
+defmodule EarmarkRawHtml do
+  @moduledoc """
+  Documentation for `EarmarkRawHtml`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EarmarkRawHtml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/earmark_raw_html_test.exs
@@ -0,0 +1,8 @@
+defmodule EarmarkRawHtmlTest do
+  use ExUnit.Case
+  doctest EarmarkRawHtml
+
+  test "greets the world" do
+    assert EarmarkRawHtml.hello() == :world
+  end
+end
